58b4558f51d5047e3090eb889afec5608893d609
Li Yang <lyang@c2r6p7.42nice.fr>
2026-04-16 20:02:18 +0200

    add: born2beroot signature

--- Born2beRoot/gnl.sh
+++ Born2beRoot/gnl.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-arch=$(uname -a)
-cpu=$(nproc)
-mem=$(free -m | awk '/Mem:/ {print $3 "/" $2}')
-disk=$(df -h --total | grep total | awk '{print $3 "/" $2}')
-boot=$(who -b | awk '{print $3 " " $4}')
-ip=$(hostname -I)
+arch=$(uname -a) # Get the system architecture
+cpu=$(nproc) # Get the number of CPU cores
+mem=$(free -m | awk '/Mem:/ {print $3 "/" $2}') # Get the used and total memory in MB
+disk=$(df -h --total | grep total | awk '{print $3 "/" $2}') # Get the used and total disk space
+boot=$(who -b | awk '{print $3 " " $4}') # Get the last boot time
+ip=$(hostname -I) # Get the IP address(es)
 
+# Display the information in a formatted way
 wall "Architecture: $arch
 CPU: $cpu
 Memory: $mem
