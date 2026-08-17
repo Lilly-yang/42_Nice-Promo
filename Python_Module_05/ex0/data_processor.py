a6210a0e7559af4f094f78fbf1c76ccb970c43a4
Li Yang <lyang@c1r1p9.42nice.fr>
2026-08-17 14:43:34 +0200

    add: python module 5 ex0

--- Python_Module_05/ex0/data_processor.py
+++ Python_Module_05/ex0/data_processor.py
@@ -7,172 +7,167 @@ class DataProcessor(abc.ABC):
         super().__init__()
         self.new_data = []
 
+    @abc.abstractmethod
     def validate(self, data: typing.Any) -> bool:
-        print(f" Trying to validate input '{data}': ", end="")
-        # if isinstance(data, int | float | list[int | float] | str | list[str] | dict[str, str] | list[dict[str, str]]):
-        #     print(True)
-        #     return True
-        # else:
-        #     print(False)
-        #     return False
+        pass
 
+    @abc.abstractmethod
     def ingest(self, data: typing.Any) -> None:
-        if isinstance(data, int | float | list[int | float] | str | list[str] | dict[str, str] | list[dict[str, str]]):
-            print(f"Processing data: {data}")
-        else:
-            print("Got exception: Improper numeric data")
-            return False
-        # try:
-        #     if isinstance(data, int | float | str):
-        #         self.new_data.append([str(data)])
-        #     elif isinstance(data, list[int | float] | list[str]):
-        #         self.new_data.append([str(i) for i in data])
-        #     elif isinstance(data, dict[str, str]):
-        #         self.new_data.append([k + ":" + v for k, v in data.items()])
-        #     elif isinstance(data, list[dict[str, str]]):
-        #         for d in data:
-        #             self.new_data.append([k + ":" + v for k, v in d.items()])
-        #     else:
-        #         raise ValueError("Invalide data dype!")
-        # except Exception as e:
-        #     print(f"e")
-        #     return False
+        pass
 
     def output(self) -> tuple[int, str]:
-        if len(self.new_data):
-            oldest_data = self.new_data.pop(0)
-            return oldest_data
-        else:
-            print("No more data.")
-            return None
+        oldest_data = self.new_data.pop(0)
+        return oldest_data
 
 
 class NumericProcessor(DataProcessor):
     def validate(self, data: typing.Any) -> bool:
-        super().validate(data)
-        if isinstance(data, int | float | list[int | float]):
+        print(f" Trying to validate input '{data}': ", end="")
+        if isinstance(data, (int, float)):
+            print(True)
+            return True
+        elif isinstance(data, list):
+            for item in data:
+                if not isinstance(item, (int, float)):
+                    print(False)
+                    return False
             print(True)
             return True
         else:
-            return(True)
+            print(False)
             return False
-        
+
     def ingest(self, data: int | float | list[int | float]) -> None:
-        super().ingest(data)
-        try:
-            if isinstance(data, int | float):
-                self.new_data.append([str(data)])
-                return True
-            elif isinstance(data, list[int | float]):
-                self.new_data.append([str(i) for i in data])
-                return True
-            else:
-                raise ValueError("Invalide data dype!")
-        except Exception as e:
-            print("Got exception: Improper numeric data")
-            return False
+        if isinstance(data, (int, float)):
+            print(f" Processing data: {data}")
+            self.new_data.append(str(data))
+        elif isinstance(data, list):
+            for item in data:
+                if not isinstance(item, (int, float)):
+                    print(" Got exception: Improper numeric data")
+                    return None
+            print(f" Processing data: {data}")
+            self.new_data.extend([str(item) for item in data])
+        else:
+            print(" Got exception: Improper numeric data")
+            return None
 
 
 class TextProcessor(DataProcessor):
     def validate(self, data: typing.Any) -> bool:
-        super().validate(data)
-        if isinstance(data, str | list[str]):
+        print(f" Trying to validate input '{data}': ", end="")
+        if isinstance(data, str):
+            print(True)
+            return True
+        elif isinstance(data, list):
+            for item in data:
+                if not isinstance(item, str):
+                    print(False)
+                    return False
             print(True)
             return True
         else:
             print(False)
             return False
-        
+
     def ingest(self, data: str | list[str]) -> None:
-        super().ingest(data)
-        try:
-            if isinstance(data, str):
-                self.new_data.append([data])
-                return True
-            elif isinstance(data, list[str]):
-                self.new_data.append(data)
-                return True
-            else:
-                raise ValueError("Invalide data dype!")
-        except:
-            print(f"e")
-            return False
+        if isinstance(data, str):
+            print(f" Processing data: {data}")
+            self.new_data.append(data)
+        elif isinstance(data, list):
+            for item in data:
+                if not isinstance(item, str):
+                    print(" Got exception: Improper numeric data")
+                    return None
+            print(f" Processing data: {data}")
+            self.new_data.extend(data)
+        else:
+            print(" Got exception: Improper numeric data")
+            return None
+
 
 class LogProcessor(DataProcessor):
     def validate(self, data: typing.Any) -> bool:
-        super().validate(data)
-        if isinstance(data, dict[str, str] | list[dict[str, str]]):
+        print(f" Trying to validate input '{data}': ", end="")
+        if isinstance(data, dict):
+            for k, v in data.items():
+                if not isinstance(k, str) or not isinstance(v, str):
+                    print(False)
+                    return False
+            print(True)
+            return True
+        elif isinstance(data, list):
+            for item in data:
+                if isinstance(item, dict):
+                    for k, v in item.items():
+                        if not isinstance(k, str) or not isinstance(v, str):
+                            print(False)
+                            return False
+                else:
+                    print(False)
+                    return False
             print(True)
             return True
         else:
             print(False)
             return False
-        
+
     def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
-        super().ingest(data)
-        try:
-            if isinstance(data, dict[str, str]):
-                self.new_data.append([k + ":" + v for k, v in data.items()])
-                return True
-            elif isinstance(data, list[dict[str, str]]):
-                for d in data:
-                    self.new_data.append([k + ":" + v for k, v in d.items()])
-                return True
-            else:
-                raise ValueError("Invalide data dype!")
-        except:
-            print(f"e")
+        if isinstance(data, dict):
+            for k, v in data.items():
+                if not isinstance(k, str) or not isinstance(v, str):
+                    return None
+            print(f" Processing data: {data}")
+            values = [v for v in data.values()]
+            self.new_data.append(": ".join(values))
+        elif isinstance(data, list):
+            for item in data:
+                if isinstance(item, dict):
+                    for k, v in item.items():
+                        if not isinstance(k, str) or not isinstance(v, str):
+                            return None
+                else:
+                    return None
+            print(f" Processing data: {data}")
+            for item in data:
+                values = [v for v in item.values()]
+                self.new_data.append(": ".join(values))
+        else:
+            print(False)
             return False
-
-
-numbers = []
-strings = []
-dicts = []
 
 
 if __name__ == "__main__":
     print("=== Code Nexus - Data Processor ===")
 
-    print("Testing Numeric Processor...")
+    print("\nTesting Numeric Processor...")
     number_processor = NumericProcessor()
-    number_processor.validate('42')
+    number_processor.validate(42)
     number_processor.validate('Hello')
 
     print(" Test invalid ingestion of string 'foo' without prior validation:")
     number_processor.ingest('foo')
 
-    number_processor.ingest( [1, 2, 3, 4, 5])
-    print("Extracting 3 values...")
+    number_processor.ingest([1, 2, 3, 4, 5])
+    print(" Extracting 3 values...")
     for i in range(3):
-        print(f"Numeric value {i}: ", end="")
-        number_processor.output()
+        print(f" Numeric value {i}: {number_processor.output()}")
 
+    print("\nTesting Text Processor...")
     string_processor = TextProcessor()
+    string_processor.validate(42)
+    string_processor.ingest(['Hello', 'Nexus', 'World'])
+    print(" Extracting 1 values...")
+    for i in range(1):
+        print(f" Text value {i}: {string_processor.output()}")
+
+    print("\nTesting Log Processor...")
     dict_processor = LogProcessor()
-
-
-    ## test validate method with valid and invalid data
-    # test NumericProcessor validate method
-
-    # test TextProcessor validate method
-
-    # test LogProcessor validate method
-
-
-    ## test ingest method with invalid data
-    # test NumericProcessor ingest method
-
-    # test TextProcessor ingest method
-
-    # test LogProcessor ingest method
-
-
-    ## ingest and output various data
-    # test NumericProcessor
-
-    # test TextProcessor
-
-    # test LogProcessor
-
-
-
+    dict_processor.validate('Hello')
+    dict_processor.ingest([
+        {'log_level': 'NOTICE', 'log_message': 'Connection to server'},
+        {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}
+        ])
+    print(" Extracting 2 values...")
+    for i in range(2):
+        print(f" Text value {i}: {dict_processor.output()}")
