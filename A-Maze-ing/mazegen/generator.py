187e411b3d25d53d104e4ef16e8d70efba6e4b0c
Lilly-yang <39558023+Lilly-yang@users.noreply.github.com>
2026-09-11 07:20:29 +0200

    add: part of maze generator

--- A-Maze-ing/mazegen/generator.py
+++ A-Maze-ing/mazegen/generator.py
@@ -1,3 +1,7 @@
+import numpy as np
+import copy
+
+
 class MazeGenerator:
     def __init__(self, width, height, seed):
         """
@@ -8,6 +12,38 @@ class MazeGenerator:
         self.width = width
         self.height = height
         self.seed = seed
+        self.creat_maze()
+
+    def creat_maze(self):
+        self.maze = []
+        for _ in range(self.height):
+            row_tmp = []
+            for _ in range(self.width):
+                cell = {
+                    "N": 1,
+                    "E": 1,
+                    "S": 1,
+                    "W": 1
+                }
+                row_tmp.append(cell)
+            self.maze.append(row_tmp)
+
+    def _get_neighbors(self, x, y):
+        nbs = []
+        if x - 1 >= 0:
+            nbs.append((x-1, y))
+
+        if x + 1 < self.width:
+            nbs.append((x+1, y))
+
+        if y - 1 >= 0:
+            nbs.append((x, y-1))
+
+        if y + 1 < self.height:
+            nbs.append((x, y+1))
+
+        return nbs
+
 
     def generate(self):
         self.maze = 
