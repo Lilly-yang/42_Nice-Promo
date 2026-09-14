7a3c986c3be7ef21064393433e318db865821f6c
Li Yang <lyang@c4r1p2.42nice.fr>
2026-09-14 15:03:55 +0200

    modify: solve dead ends more than 2 limitation

--- A-Maze-ing/mazegen/generator.py
+++ A-Maze-ing/mazegen/generator.py
@@ -1,5 +1,6 @@
 import random
 import sys
+from itertools import cycle
 
 
 class MazeGenerator:
@@ -20,7 +21,7 @@ class MazeGenerator:
         self.output_file = output_file
         self.seed = seed
         self.icon42 = icon42
-        self.solution = ""
+        self.solution: list[tuple] = []
         self.icon_cell: set[tuple] = set()
 
     def _init_maze(self) -> None:
@@ -50,6 +51,7 @@ class MazeGenerator:
 
         if self.entry in self.icon_cell or self.exit in self.icon_cell:
             self.icon42 = False
+            self.icon_cell.clear()
             print("Error: 42 pattern is omitter "
                   "because cell is occupied with entry or exit!")
 
@@ -79,7 +81,7 @@ class MazeGenerator:
             neighbors = self._get_neighbors(start[0], start[1])
             valid_nb = neighbors - visited
             if valid_nb:
-                next_cell = self.rng.choice(list(valid_nb))
+                next_cell = self.rng.choice(sorted(valid_nb))
                 self._remove_wall(start, next_cell)
                 start = next_cell
                 stack.append(start)
@@ -137,29 +139,100 @@ class MazeGenerator:
                 self.maze[end_h][end_w]["N"] = 0
 
     def _loop_gen(self) -> None:
-        n = self.width * self.height // 10 + 1
+        dead_ends = self._get_dead_ends()
+        dead_ends_before = -1
+        dead_ends_now = len(dead_ends)
+        open_drc = cycle(["SE", "NW"])
+
+        while dead_ends_now > 2 and dead_ends_now != dead_ends_before:
+            print(f"dead ends: {len(dead_ends)}")
+
+            dead_ends_before = dead_ends_now
+            n = int(dead_ends_now / 2) + 1
+            candidate = self._get_cell_to_open(dead_ends, next(open_drc))
+
+            for _ in range(n):
+                if not candidate:
+                    break
+
+                h, w, drc = self.rng.choice(candidate)
+                candidate.remove((h, w, drc))
+
+                self.maze[h][w][drc] = 0
+
+                if drc == "S":
+                    self.maze[h+1][w]["N"] = 0
+
+                if drc == "E":
+                    self.maze[h][w+1]["W"] = 0
+
+                if drc == "N":
+                    self.maze[h-1][w]["S"] = 0
+
+                if drc == "W":
+                    self.maze[h][w-1]["E"] = 0
+
+            dead_ends = self._get_dead_ends()
+            dead_ends_now = len(dead_ends)
+
+        print(f"finale dead ends: {dead_ends_now}")
+
+    def _get_cell_to_open(self,
+                          org_cells: list,
+                          direction: str = "SE") -> list:
+        # print(f"---getting cells that avaliable to open {direction}---")
         candidate = []
 
-        for h in range(0, self.height):
-            for w in range(0, self.width):
-                if self.maze[h][w]["S"] and h < self.height - 1:
+        if direction == "SE":
+            for w, h in org_cells:
+                if (
+                    h < self.height - 1
+                    and (w, h+1) not in self.icon_cell
+                    and self.maze[h][w]["S"] == 1
+                ):
                     candidate.append((h, w, "S"))
-                if self.maze[h][w]["E"] and w < self.width - 1:
+                if (
+                    w < self.width - 1
+                    and (w+1, h) not in self.icon_cell
+                    and self.maze[h][w]["E"] == 1
+                ):
                     candidate.append((h, w, "E"))
 
-        if n > len(candidate):
-            n = len(candidate)
+        if direction == "NW":
+            for w, h in org_cells:
+                if (
+                    h
+                    and (w, h-1) not in self.icon_cell
+                    and self.maze[h][w]["N"] == 1
+                ):
+                    candidate.append((h, w, "N"))
+                if (
+                    w
+                    and (w-1, h) not in self.icon_cell
+                    and self.maze[h][w]["W"] == 1
+                ):
+                    candidate.append((h, w, "W"))
 
-        for _ in range(n):
-            h, w, drc = self.rng.choice(candidate)
-            self.maze[h][w][drc] = 0
-            if drc == "S":
-                self.maze[h+1][w]["N"] = 0
+        return candidate
 
-            if drc == "E":
-                self.maze[h][w+1]["W"] = 0
+    def _get_dead_ends(self) -> list:
+        dead_ends = []
 
-            candidate.remove((h, w, drc))
+        for h in range(self.height):
+            for w in range(self.width):
+                if (w, h) in self.icon_cell:
+                    continue
+
+                open_count = 0
+
+                for direction in ["N", "E", "S", "W"]:
+                    if self.maze[h][w][direction] == 0:
+                        open_count += 1
+
+                if open_count == 1:
+                    dead_ends.append((w, h))
+
+        return dead_ends
 
     def solve(self) -> list:
         print("solving maze...")
@@ -172,7 +245,7 @@ class MazeGenerator:
         visited.add(start)
         while True:
             if start == self.exit:
-                # self.stack = stack
+                self.solution = stack
                 # print(f"maze solved: {''.join(solution[:10])}", end="")
                 # if len(solution) > 10:
                 #     print("...")
@@ -181,7 +254,7 @@ class MazeGenerator:
             neighbors = self._get_open_neighbors(start[0], start[1])
             valid_nb = neighbors - visited
             if valid_nb:
-                start = self.rng.choice(list(valid_nb))
+                start = self.rng.choice(sorted(valid_nb))
                 # solution.append(self._get_direction(start, next_cell))
                 # start = next_cell
                 stack.append(start)
@@ -239,31 +312,35 @@ class MazeGenerator:
 
     def output_maze(self) -> None:
         from pathlib import Path
-        import os
         current_dir = Path(__file__).resolve().parent.parent
-        output_path = os.path.join(current_dir, self.output_file)
+        output_path = current_dir / self.output_file
         with open(output_path, "w") as f:
             for h in range(self.height):
                 line = ""
                 for w in range(self.width):
                     line += self._direction_hexadecimal(self.maze[h][w])
-                line += '\n'
-                f.write(line)
+                f.write(line + "\n")
 
             f.write('\n')
             f.write(str(self.entry[0])+","+str(self.entry[1])+"\n")
             f.write(str(self.exit[0])+","+str(self.exit[1])+"\n")
-            if self.solution == "":
-                solution = self.solve()
-            f.write(self._get_direction(solution)+"\n")
+            if not len(self.solution):
+                self.solution = self.solve()
+            f.write(self._get_direction(self.solution)+"\n")
 
-            f.close()
         print(f"maze saved in: {output_path}")
 
     def _direction_hexadecimal(self, cell: dict) -> str:
-        dec_cell = 0
-        for ind, drc in enumerate(["W", "S", "E", "N"]):
-            dec_cell += cell[drc] * (2 ** ind)
+        dec_cell = (
+            cell["N"]
+            + cell["E"] * 2
+            + cell["S"] * 4
+            + cell["W"] * 8
+        )
 
-        hex = "0123456789ABCDEF"
-        return hex[dec_cell]
+        # dec_cell = 0
+        # for ind, drc in enumerate(["W", "S", "E", "N"]):
+        #     dec_cell += cell[drc] * (2 ** ind)
+
+        hex_digits = "0123456789ABCDEF"
+        return hex_digits[dec_cell]
